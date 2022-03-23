38c7e6530a8753b845cbaa904eaf942f5e4b0783
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2022-03-23 17:57:47 +1300

    replace formatId function with direct schema transformation in RestaurantModel

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const Restaurant = require("./models/RestaurantModel")
+const RestaurantModel = require("./models/RestaurantModel");
 
 app.use(cors());
 app.use(express.json());
 
-
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await RestaurantModel.find({});
+  return res.status(200).send(restaurants);
+});
 
 module.exports = app;
--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -9,4 +9,17 @@ const restaurantSchema = new Schema({
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
+// CITATION:
+// Description: transform mongoose schema data directly to get rid of _id and replace with id.
+// Title of post: "mongoose "_id" field can't be deleted".
+// Date posted: 2015-04-02
+// URL: https://stackoverflow.com/questions/29407567/mongoose-id-field-cant-be-deleted
+
+restaurantSchema.options.toJSON = {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+};
+
 module.exports = Restaurant;
