8eed0d5a4b04f09c6c43a6096e9614f18c187999
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2022-03-23 14:47:58 +1300

    add restaurant schema

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,1 +1,12 @@
-// FIXME: Add a Mongoose model here
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const restaurantSchema = new Schema({
+  name: { type: String, required: true },
+  description: String,
+  Image: String,
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = Restaurant;
