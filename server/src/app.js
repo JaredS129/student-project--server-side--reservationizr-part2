220c2d893da3bcab455e2e47996271447cda2dea
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2022-03-24 12:58:54 +1300

    add validId.js in utils

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,21 @@ app.get("/restaurants", async (req, res) => {
   return res.status(200).send(restaurants);
 });
 
-app.get("*", async (req, res) => {
-  return res.status(404).send({ message: "page not found" });
-});
+// app.get("/restaurants/:id", async (req, res) => {
+//     const id = req.params.id;
+//     if (validId(id) === false) {
+//       return res.status(400).send({ message: "id provided is invalid" });
+//     }
+//     const reservation = await ReservationModel.findById(id);
+//     if (reservation === null) {
+//       return res.status(404).send({ message: "id not found" });
+//     }
+//     const formattedReservation = formatReservation(reservation);
+//     return res.status(200).send(formattedReservation);
+//   });
+
+// app.get("*", async (req, res) => {
+//   return res.status(404).send({ message: "page not found" });
+// });
 
 module.exports = app;
